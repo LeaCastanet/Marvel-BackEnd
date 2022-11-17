0c7ff2139176ac78c779a328b4f117580b84d26d
Léa Castanet <lea@mbp-de-lea.lan>
2022-11-17 17:27:50 +0100

    ajout limit et page

--- routes/characters.js
+++ routes/characters.js
@@ -5,13 +5,39 @@ require("dotenv").config();
 
 router.get("/characters", async (req, res) => {
   try {
-    const limit = "";
-    const name = "";
+    // let limit = 100;
+    // if (req.query.limit) {
+    //   limit = req.query.limit;
+    // }
+
+    const limit = req.query.limit || 100;
+    // const skip = req.query.skip || 0;
+    const name = req.query.name || "";
+    const page = req.query.page || 1;
+
+    let pageRequired = 1;
+    if (page) {
+      pageRequired = Number(page);
+    }
+    const skip = (pageRequired - 1) * limit;
+
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.APIKEY}&${limit}&${name}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.APIKEY}&limit=${limit}&skip=${skip}&name=${name}`
     );
     res.json(response.data);
     console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.get("/character/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.APIKEY}`
+    );
+
+    res.json(response.data);
   } catch (error) {
     console.log(error.message);
   }
--- routes/comics.js
+++ routes/comics.js
@@ -5,11 +5,21 @@ require("dotenv").config();
 
 router.get("/comics", async (req, res) => {
   try {
-    const limit = "";
-    const title = "";
+    const limit = req.query.limit || 100;
+    // const skip = req.query.skip || 0;
+    const title = req.query.title || "";
+    const page = req.query.page || 1;
+
+    let pageRequired = 1;
+    if (page) {
+      pageRequired = Number(page);
+    }
+    const skip = (pageRequired - 1) * limit;
+
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.APIKEY}&limit=${limit}&title${title}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.APIKEY}&limit=${limit}&skip=${skip}&title=${title}`
     );
+
     res.json(response.data);
 
     // console.log(response.data);
